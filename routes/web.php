87c2819ba1656796012770500adb011e87b53390
abhista <abhistapsava@gmail.com>
2026-09-11 09:44:04 +0700

    feat: tambah tugas satu per satu ke daftar (FR-02)
    - Tambah model & migration Task dengan referensi ke task_list
    - Tambah halaman tambah tugas

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,15 @@
 <?php
 
+use App\Http\Controllers\TaskController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/tasks', function () {
+    return view('tasks.index', ['tasks' => \App\Models\Task::latest()->get()]);
+});
+
+Route::get('/tasks/create', [TaskController::class, 'create']);
+Route::post('/tasks', [TaskController::class, 'store']);
